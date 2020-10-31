20b789970aba71faf957281a3aa7707fb68e6627
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-10-31 10:26:48 +0900

    1031
    1031

--- test/fstest.js
+++ test/fstest.js
@@ -10,8 +10,12 @@ util.log('1111111111111');
 let data2 = fs.readFileSync('test.json','utf-8');
 util.log('data2 : ',data2)
 util.log("222222222222222")
-fs.writeFile('message.txt', 'Hello Node.js', (err) => {
+// fs.writeFile('message.txt', 'Hello Node.js', (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+//     let data3 = fs.readFileSync('message.txt','utf-8');
+// });
+fs.appendFile('message.txt', 'data to append2', (err) => {
     if (err) throw err;
-    console.log('The file has been saved!');
-    let data3 = fs.readFileSync('message.txt','utf-8');
-});
+    console.log('The "data to append" was appended to file!');
+  });

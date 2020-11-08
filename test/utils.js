b357fe4d9114e81a4036a182ca84eb16df4b4b7b
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-11-08 15:35:50 +0900

    1108
    1108

--- test/utils.js
+++ test/utils.js
@@ -8,8 +8,18 @@ SHA256 = ('crypto-js/sha256');
 //thread를 safe하게 할 때 사용.
 //static하면 메소드를 동시에 둘이서 콜했을 때 영향을 줄 수 있는 상황이라면 문제가 된다.
 //이때 class로 만들어준다.
+const Ekey = "nodevue";
+
 module.exports={
     //이미지를 담은 url을 가져오는 함수
+    encrypt(data,key){
+        return Crypto.AES.encrypt(data,key || Ekey).toString();
+    },
+    decrtpt(data,key){
+        return Crypto.AES.decrypt(data,key || Ekey).toString(Crypto.enc.Utf8);
+    }
+    ,
+
     ogsinfo(url,fn){
         return ogs({url : url},(err,ret)=>{
             fn(err,ret);
--- test/utilstest.js
+++ test/utilstest.js
@@ -1,5 +1,14 @@
 const utils = require('./utils')
 
+let str = "hello";
+let key = "hi"
+let enc = utils.encrypt(str,key)
+console.log(enc);
+let dec=utils.decrtpt(enc,key)
+console.log(dec);
+
+
+return
 let url ='https://www.naver.com/';
 utils.ogsinfo(url,(err,ret)=>{
     console.log(err,ret.ogImage.url);

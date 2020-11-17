f009cf5287ca097700809c0138e4a17d3d64f14c
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-11-17 16:03:43 +0900

    1117
    1117

--- test/promise.js
+++ test/promise.js
@@ -1,16 +1,52 @@
-new Promise((resolve, reject) => {
-    console.log(1);
+const { resolve } = require("bluebird");
 
-    resolve();
+// function getNum() {
+//     return new Promise(function (resolve, reject) {
+//         var num = 10;
+//         resolve(num)
+//     });
+// }
+// getNum().then(function (resolvednum) {
+//     console.log(2)
+//     console.log(resolvednum);
+//     resolve(1)
+
+// }).then((resolvednum_1) => {
+//     console.log(resolvednum_1)
+//     console.log(3)
+// })
+
+// function getError() {
+//     return new Promise(function (resolve, reject) {
+//         var num = 10;
+//         reject(new Error("reject"))
+//     });
+// }
+// getError().then(function (resolvednum) {
+//     console.log(resolvednum);
+// }).catch(function(err){
+//     console.log(1)
+//     console.log(err)
+// })
+
+function getNum() {
+    return new Promise(function (resolve, reject) {
+        var num = 10;
+        if (num==10){
+            resolve(num)
+        }else{
+            reject(new Error('not 10'))
+        }
+    });
+}
+getNum().then(function (resolvednum) {
+    console.log(resolvednum);
+    return(resolvednum);
+}).catch((err) => {
+    console.log(err)
+}).then(function(resolvednum_1){
+    console.log(resolvednum_1)
+    resol
+}).then(()=>{
+    console.log(1)
 })
-.then(() => {
-    throw new Error('Something failed');
-        
-    console.log(2);
-})
-.catch(() => {
-    console.log(3);
-})
-.then(() => {
-    console.log(4);
-});
